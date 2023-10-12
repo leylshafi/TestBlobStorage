28b97aaa91373718ca65152fdbfaf6d747410b69
Leyla Shafiyeva <llshafi03@gmail.com>
2023-10-12 23:43:50 +0400

    bug fix

--- TestBlobStorage/Models/Dtos/UserDto.cs
+++ TestBlobStorage/Models/Dtos/UserDto.cs
@@ -7,6 +7,5 @@
         public string Username { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
         public int Age { get; set; }
-        public string ProfilePhoto { get; set; }
     }
 }
